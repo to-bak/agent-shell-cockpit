b6cadf39d085c020d724e6ff30a94da83d8bd4b7
to-bak <7633828+to-bak@users.noreply.github.com>
2026-09-08 22:38:55 +0200

    update package-smoke

--- test/package-smoke.el
+++ test/package-smoke.el
@@ -27,10 +27,19 @@
         ;; Register the real dependencies already supplied by the test runner.
         ;; No package archives or user package configuration are consulted.
         (dolist (dependency '(agent-shell magit-section transient))
-          (let* ((library (locate-library (symbol-name dependency)))
-                 (file (concat (file-name-sans-extension library) ".el"))
+          (let* ((library (or (locate-library (symbol-name dependency))
+                              (error "Could not locate dependency: %s" dependency)))
+                 ;; Emacs 31's bundled transient is installed as transient.el.gz.
+                 ;; `locate-library' finds transient.elc, so also accept compressed
+                 ;; source when reading its package header.
+                 (source (concat (file-name-sans-extension library) ".el"))
+                 (file (cond ((file-exists-p source) source)
+                             ((file-exists-p (concat source ".gz"))
+                              (concat source ".gz"))
+                             (t (error "Could not find source for dependency: %s"
+                                       dependency))))
                  (version (with-temp-buffer (insert-file-contents file)
-					    (or (lm-header "package-version") (lm-header "version")))))
+			    (or (lm-header "package-version") (lm-header "version")))))
             (push (cons dependency
                         (list (package-desc-create :name dependency :version (version-to-list version)
                                                    :dir (file-name-directory library)))) package-alist)))
