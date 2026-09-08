6f79d4239a230685351cd00adef5d1191290d8e3
to-bak <7633828+to-bak@users.noreply.github.com>
2026-09-08 22:42:30 +0200

    fix test

--- test/lint-dependencies.el
+++ test/lint-dependencies.el
@@ -1,11 +1,21 @@
 ;;; lint-dependencies.el --- Describe installed checker dependencies -*- lexical-binding: t; -*-
 (require 'package)
 (require 'lisp-mnt)
+(defun agent-shell-cockpit-test-dependency-source-file (dependency)
+  "Return the readable source file for installed DEPENDENCY."
+  (let* ((library (or (locate-library (symbol-name dependency))
+                      (error "Could not locate dependency: %s" dependency)))
+         (source (concat (file-name-sans-extension library) ".el")))
+    ;; Emacs 31 installs bundled transient source as transient.el.gz.
+    (cond ((file-exists-p source) source)
+          ((file-exists-p (concat source ".gz")) (concat source ".gz"))
+          (t (error "Could not find source for dependency: %s" dependency)))))
+
 (dolist (dependency '(agent-shell magit-section transient))
   (let* ((library (locate-library (symbol-name dependency)))
-         (file (concat (file-name-sans-extension library) ".el"))
+         (file (agent-shell-cockpit-test-dependency-source-file dependency))
          (version (with-temp-buffer (insert-file-contents file)
-				    (or (lm-header "package-version") (lm-header "version")))))
+			    (or (lm-header "package-version") (lm-header "version")))))
     (push (cons dependency
                 (list (package-desc-create :name dependency :version (version-to-list version)
                                            :dir (file-name-directory library)))) package-alist)))
